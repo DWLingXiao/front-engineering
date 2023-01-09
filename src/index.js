8be97f5a6166a624a179def9bc167ac39dbf64ae
DWLingXiao <3067976737@qq.com>
2023-01-09 19:38:45 +0800

    feat(新增commit规范): 初始化项目,新增commit提交规范

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { NodeType } from "@yangzw/bruce-us/dist/node";
+// import { readFileSync } from "fs";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// console.log(__filename, __dirname);
+console.log(NodeType());
+console.log(111)
+
+// const json = readFileSync("./info.json");
+// const info = JSON.parse(json);
+// console.log(info)
